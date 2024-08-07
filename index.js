fa26d7a1fb5c3206061a2f14751363417b8da31e
sarvarbekovich <khasanboyrumonov1998@yahoo.com>
2024-08-08 03:28:06 +0900

    patirning narxini yozb ozgartrdim

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const c = KhASANBOY (15, 25)
     weight: '50kg',
     type: 'middle size',
     introduce() { console.log( `I would like to buy ${this.name} that weighs ${this.weight} 
-        for  my all money at ${this.price} because its size is ${this.type} `);}
+        for  my all money at ${this.price} because its size is ${this.type} `);},
 
+    sotamiz() { console.log(`${this.name}ning narxi yozdan boshlab ${this.price}`);}
   }
   bread.introduce();
+  bread.sotamiz();

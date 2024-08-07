46e600255adbe8941dfde68a995e87ab5a3f9ead
sarvarbekovich <khasanboyrumonov1998@yahoo.com>
2024-08-08 03:06:40 +0900

    i am doing vs code

---
+++ index.js
@@ -0,0 +1,16 @@
+/*function KhASANBOY ( a, b) {
+    return a * b;
+}
+const c = KhASANBOY (15, 25)
+*/
+// console.log( 'result:', c);
+  const bread = { 
+    name: 'PATIR',
+    price: 30,
+    weight: '50kg',
+    type: 'middle size',
+    introduce() { console.log( `I would like to buy ${this.name} that weighs ${this.weight} 
+        for  my all money at ${this.price} because its size is ${this.type} `);}
+
+  }
+  bread.introduce();
